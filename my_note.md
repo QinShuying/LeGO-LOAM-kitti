b72a9fe430d6f9fcbc121e7740cef36192ace8f5
QinShuying <qsy20000310@126.com>
2020-12-22 16:32:01 +0800

    kitti_pose

--- my_note.md
+++ my_note.md
@@ -4,10 +4,12 @@
     $ cd ~/catkin_ws/
     $ catkin_make #初始化工作空间
 
-#### package的创建
+#### package创建
     /catkin_ws/src$ catkin_create_pkg test_pkg roscpp rospy std_msgs
 填充　CMakeLists.txt　和　package.xml,　并且将依赖项填进了这两个文件中
 
+
+------
 #### CMakeLists.txt / Boost
     aux_source_directory(${PROJECT_SOURCE_DIR}/src SRC_DIR)
     add_library(${PROJECT_NAME} SHARED
@@ -31,14 +33,54 @@
     <node pkg="kitti_pkg" type="kitti2pcd"    name="kitti2pcd"    output="screen"/>
 - type = 节点名字 = 可执行文件名字
 - name = 命名空间
+    
+#### my.rviz 
+- 第一次运行rviz 配置后可自动生成 .rviz文件
 
-##### 启动node
+
+-------
+#### 启动node
     src$ roscore
     src$ rosrun pkg_name node_name
     或
     src$ roslaunch kitti_pkg run.launch
 
-##### 终端
+#### 终端
     $ gedit ~/.bashrc
     
     source /home/qsy-5208/Documents/LeGO/LeGO-LOAM-kitti/catkin_ws/devel/setup.bash
+
+#### Topic确认 / Debug
+    $ rostopic echo 
+
+
+-----
+#### 时间同步
+错误信息（from rostopic）：
+    
+    WARNING: no messages received and simulated time is active.
+    Is /clock being published?
+    
+解决：
+
+    run.launch ———— <param name="/use_sim_time" value="false" />   
+    .cpp ———— output.header.stamp = ros::Time::now();
+
+------
+#### Github
+~/Documents/LeGO/LeGO-LOAM-kitti/catkin_ws/src/kitti_pkg$
+
+    $ git reset
+    $ git add -A
+    $ git status
+    $ git commit -m ""init
+    
+    $ git remote add origin https://github.com/QinShuying/LeGO-LOAM-kitti.git
+    $ git push origin master
+
+------
+#### Edit Configration
+若在CMake中 add_executable( .cpp)
+但Edit Configration找不到，则手动添加
+
+    Edit Configration --> '+' --> CMake Applilcation --> Target 
--- nodecpp/kitti2pcd.cpp
+++ nodecpp/kitti2pcd.cpp
@@ -1,4 +1,3 @@
-
 #include "fstream"
 #include "iostream"
 #include <ctime>
@@ -19,13 +18,13 @@
 #include <pcl/registration/icp.h>
 
 
-//#include "fcn_data_gen/ground_remove.h"
 using namespace std;
 typedef pcl::PointXYZI PointType;
 
-static ros::Publisher g_cloud_pub;
-static std::vector<std::string> file_lists;
+static ros::Publisher pcl_pub;
 vector<string> binfiles;
+
+pcl::PointCloud<PointType> cloud;
 
 
 bool exists_file(const std::string &name) {
@@ -36,7 +35,7 @@ bool exists_file(const std::string &name) {
 void Load_binfile() {
     cout << "Start Load" << endl;
 
-    string strPath = "/media/qsy-5208/Resource/SLAM_DATA/Datasets/KITTI_odometry/data_odometry_velodyne/sequences/1-10/01/velodyne";    // 109 + 6 +4
+    string strPath = "/media/qsy-5208/Resource/SLAM_DATA/Datasets/KITTI_odometry/data_odometry_velodyne/sequences/1-10/00/velodyne";    // 109 + 6 +4
 
     int img_i = -1;
     do {
@@ -80,45 +79,51 @@ void readKittiPclBinData(std::string &in_file, std::string& out_file)
     }
     input.close();
 
-//    g_cloud_pub.publish(points);
+    // Method1：直接从bin文件读取点，发布点云
+    cloud = *points;    // 点-->可发布的点云
 
 
-//    Save pcl outputs
+////    Save pcl outputs
 //    std::cout << "Read KTTI point cloud with " << i << " points, writing to " << out_file << std::endl;
 //    pcl::PCDWriter writer;
 //    writer.write< PointType > (out_file, *points, false);
 }
 
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "kitti2pcd");
     ros::NodeHandle nh("~");
+    ros::Rate rate(500);
 
-    std::string path;
     ROS_INFO("ROS Started.");
-    g_cloud_pub = nh.advertise< pcl::PointCloud< PointType > > ("point_chatter", 1);
 
-    ros::Publisher pcl_pub = nh.advertise<sensor_msgs::PointCloud2>("pcl_pub", 10);
-
+    pcl_pub = nh.advertise<sensor_msgs::PointCloud2>("pcl_pub", 10);
 
     Load_binfile();
-    std::string pcd_path = "/home/qsy-5208/Documents/LeGO/LeGO-LOAM-kitti/kitti_pcd_01/";
-    ros::Rate rate(200);
+
+    std::string pcd_path = "/home/qsy-5208/Documents/LeGO/LeGO-LOAM-kitti/kitti_pcd_00/";
+
     int i = 0;
     while (ros::ok())
     {
+        // .bin-->.pcd
         std::string bin_file = binfiles[i];
         std::string tmp_str = binfiles[i].substr(109, binfiles[i].length() - 109 - 4) + ".pcd";
         std::string pcd_file = pcd_path + tmp_str;
+        readKittiPclBinData(bin_file, pcd_file);
 
-        pcl::PointCloud<PointType> cloud;
-        pcl::io::loadPCDFile(pcd_file, cloud);
+        // Method2：读取pcd文件，发布点云
+//        pcl::PointCloud<PointType> cloud;
+//        pcl::io::loadPCDFile(pcd_file, cloud);
+
         sensor_msgs::PointCloud2 output;
-        pcl::toROSMsg(cloud, output);// 转换成ROS下的数据类型 最终通过topic发布
+        pcl::toROSMsg(cloud, output);   // 转换成ROS下的数据类型 最终通过topic发布
         output.header.stamp = ros::Time::now();
         output.header.frame_id = "slam";
         pcl_pub.publish(output);
         rate.sleep();
+
         i++;
         if(i==binfiles.size())
             break;
